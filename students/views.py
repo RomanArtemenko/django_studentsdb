a0c47127847a801291122924e7c2b4c93fc52d80
Roman Artemenko <RGArtemenko@gmail.com>
2016-03-16 23:08:02 +0200

    create template/students added students_list.html

--- students/views.py
+++ students/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import Http404
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 # Create your views here.
+
+def students_list(request):
+    #return HttpResponse('<h1>Hello World</h1>')
+    return render(request,'students/students_list.html', {})
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # Students url
+    #url(r'^students/', include('students/urls.py')),
+    url(r'^$', 'students.views.students_list', name='home'),
+    url(r'^admin/', include(admin.site.urls)),
 ]
